bc3512773c42e01097c59421c83b845544372833
Umagowda1232 <umavgowda22@gmail.com>
2023-11-06 15:19:41 +0530

    Intro to Middlewares

--- BackEnd/M2_expressJS/app.js
+++ BackEnd/M2_expressJS/app.js
@@ -1,16 +1,33 @@
 const express = require('express');
 //import express from "express";(if we use type: module in package.json then use this line orelse use the upper line)
 
+const bodyParser = require('body-parser');
+
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next();
+// app.use((req, res, next) => {
+//     console.log('In the middleware!');
+//     next();
+// });
+
+// app.use('/', (req, res, next) => {
+//     console.log('The page aways runs!');
+//     next();
+// });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+    //console.log('In Another middleware');
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
 });
 
+app.post('/product', (req, res, next) => {
+    console.log(req.body);
+    res.redirect('/');
+});
 
-app.use((req, res, next) => {
-    console.log('In Another middleware');
+app.use('/', (req, res, next) => {
     res.send('<h1>Hello FRom Express!</h1>');
 });
 
