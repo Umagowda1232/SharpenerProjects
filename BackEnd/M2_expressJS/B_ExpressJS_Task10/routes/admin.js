0a2521e5a92fc8f92b1e4c16e4a96dff6b63ad96
Umagowda1232 <umavgowda22@gmail.com>
2023-11-25 19:14:02 +0530

    B_ExpressJS_Task10_dynamicRoutesPart3

---
+++ BackEnd/M2_expressJS/B_ExpressJS_Task10/routes/admin.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const express = require('express');
+
+const adminController = require('../B_ExpressJS_Task10/controllers/admin');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
+
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
+
+router.get('/edit-product/:productId', adminController.getEditProduct);
+
+router.post('/edit-product', adminController.postEditProduct);
+
+module.exports = router;
